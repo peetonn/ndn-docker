f1d3e2a1ce8412f02c2a9e39e614e0e44c89e4a9
peetonn <gpeetonn@gmail.com>
2018-04-05 16:35:32 -0300

    added v3 tests setup

---
+++ compose/app/bootstrap.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+echo "" > $ENTRYPOINT_LOG
+echo "" > $BOOTSTRAP_LOG
+
+NFD_START_FG_CMD="nfd > ${LOG_FILE} 2>&1"
+NFD_START_BG_CMD="nfd > ${LOG_FILE} 2>&1 &"
+nfdBackground="yes"
+register=$REGISTER
+execute=$ENTRYPOINT
+
+function log {
+    echo $1 >> $BOOTSTRAP_LOG
+    echo $1
+}
+
+while getopts ":r:e:b:" opt; do
+  case $opt in
+    r) register="$OPTARG"
+    ;;
+    e) execute="$OPTARG"
+    ;;
+    b) nfdBackground="$OPTARG"
+    ;;
+    \?) log "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+function runCmd() {
+    log "$1"
+    eval $1 >> $BOOTSTRAP_LOG 2>&1
+}
+
+function registerPrefix() {
+    host=$1
+    route=$2
+
+    faceId=`nfdc face create udp://$host | sed "s/^face-created id=\([0-9][0-9]*\) .*$/\1/g"`
+    log "registering $route towards $host (face id $faceId)"
+    runCmd "nfdc route add $route $faceId"
+}
+
+function startNfd() {
+    runCmd "$NFD_START_BG_CMD"
+}
+
+log "Bootstrapping."
+log "Register: $register"
+log "Execute: $execute"
+log "NFD background: $nfdBackground"
+
+log "staring NFD..."
+startNfd
+sleep 2
+log "NFD started"
+
+if [[ ! -z $register ]]; then
+    OLDIFS=$IFS
+    for p in $register; do IFS=':';
+        set -- $p;
+        IFS=$OLDIFS;
+        registerPrefix "$1" "$2"
+    done; IFS=$OLDIFS
+fi
+
+if [[ ! -z "$execute" ]]; then
+    eval "$execute"  >> $ENTRYPOINT_LOG 2>&1
+fi
+
+if [ "$nfdBackground" == "no" ]; then
+    eval "tail -f ${LOG_FILE}" >> $ENTRYPOINT_LOG 2>&1
+fi
